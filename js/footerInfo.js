9a65b6cba468e01152faef2eba1674c352eedb63
Sergey <serzh.kachalyuk@gmail.com>
2023-04-26 23:35:04 +0300

    added footer info got from XML-file

--- js/footerInfo.js
+++ js/footerInfo.js
@@ -1,6 +1,7 @@
 $(function () {
     let
-        content = $('.footer__info'), // ссылка на контейнер
+        container = $('.footer__info'), // ссылка на контейнер
+        content = "" // html внутри контейнера
         tasksFile = "https://raw.githubusercontent.com/17Sergey/alpha/main/xml/contacts.xml"; // файл с задачами
 
     // GET запрос на получение данных из xml файла
@@ -11,39 +12,15 @@ $(function () {
         dataType: 'xml',
         async: true,
         success: (resp) => {
-            console.log("resp: ", resp)
             // найти все и пройтись
             // сформировав заполненный данными html шаблон
-            $(resp).find('task').each((index, item) => {
 
-                // получаем ссылку на задачу
-                const _item = $(item);
-
-                // и вытаскиваем из нее данные
-                const
-                    id = _item.attr('id'),
-                    name = _item.find('name').text().trim(),
-                    desc = _item.find('description').text().trim()
-
-                console.log(id)
-                console.log(name);
-                console.log(desc);
-
-                // формируем шаблон
-                content += `
-                    <tr>
-                        <th scope="row">${id}</th>
-                        <td class="tr-edit">${name}</td>
-                        <td class="tr-edit">${desc}</td>
-                    </tr>
-                `
-            });
+            $(resp).find("contacts").children().each((index, item) => {
+                content += `<p>${item.textContent}</p>`;
+            })
 
             // добавляем в текст страницы содержимое базового шаблона
-            content.html(tasksTable.content);
-            let tasks = $('#tasks');
-
-            tasks.find('tbody').html(tableRow)
+            container.html(content);
         },
 
         // ошибку выводим в консоль
