1d21e12045d77b6147fc177f99bcf18ff4361a75
Sergey <serzh.kachalyuk@gmail.com>
2023-04-30 22:12:34 +0300

    total fixing!!

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+
+
+    // Flying bullet
+    $(".logo").mouseover(function () {
+
+        let $bullet = $(".logo-bullet");
+
+        // Animation starts
+        $bullet.fadeIn();
+        $bullet.animate({ left: "-40px" }, 300);
+
+        // After animation
+        $bullet.fadeOut(500);
+        $bullet.css("left", "20px");
+    })
+
+
     $(".header__burger").click(
         function () {
             $(".header__menu").slideToggle();
